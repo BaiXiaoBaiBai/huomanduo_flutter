21404c86a069ede83174cb312d64257e7fc56821
xiaobai <786414270@qq.com>
2022-04-13 17:30:48 +0800

    1

--- lib/pages/home/controller/amap_config.dart
+++ lib/pages/home/controller/amap_config.dart
@@ -4,13 +4,16 @@ import 'package:amap_flutter_map/amap_flutter_map.dart';
 
 class AmapConfig {
 
+  static const String androidKey = "bd5dcfa63f98d2d1bfe69899a6c2377d";
+  static const String iosKey = "229731fbfb1d9faefcdc268bae9f8159";
+
   ///配置您申请的apikey，在此处配置之后，可以在初始化[AMapWidget]时，通过`apiKey`属性设置
   ///
   ///注意：使用[AMapWidget]的`apiKey`属性设置的key的优先级高于通过Native配置key的优先级，
   ///使用[AMapWidget]的`apiKey`属性配置后Native配置的key将失效，请根据实际情况选择使用
   static const AMapApiKey amapApiKeys = AMapApiKey(
-      androidKey: "bd5dcfa63f98d2d1bfe69899a6c2377d",
-      iosKey: "229731fbfb1d9faefcdc268bae9f8159"
+      androidKey: androidKey,
+      iosKey: iosKey
   );
 
   ///高德隐私合规声明，这里只是示例，实际使用中请按照实际参数设置[AMapPrivacyStatement]的'hasContains''hasShow''hasAgree'这三个参数
@@ -22,4 +25,6 @@ class AmapConfig {
   /// 高德SDK合规使用方案请参考：https://lbs.amap.com/news/sdkhgsy
   static const AMapPrivacyStatement amapPrivacyStatement =
   AMapPrivacyStatement(hasContains: true, hasShow: true, hasAgree: true);
+
+
 }
--- lib/pages/home/controller/select_location.dart
+++ lib/pages/home/controller/select_location.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -7,6 +9,9 @@ import 'package:huomanduo_owner/pages/home/controller/amap_config.dart';
 
 import '../../../common/base_app_bar.dart';
 import 'package:amap_flutter_map/amap_flutter_map.dart';
+import 'package:amap_flutter_base/amap_flutter_base.dart';
+import 'package:amap_flutter_location/amap_flutter_location.dart';
+import 'package:amap_flutter_location/amap_location_option.dart';
 
 class SelectLocation extends StatefulWidget {
   @override
@@ -16,18 +21,96 @@ class SelectLocation extends StatefulWidget {
 class _SelectLocationState extends State<SelectLocation>
     with SingleTickerProviderStateMixin {
 
+  LatLng _mapCenter = LatLng(39.909187, 116.397451);
+  AMapFlutterLocation _locationPlugin = new AMapFlutterLocation();
+
   @override
   void initState() {
 
+    AMapFlutterLocation.setApiKey(AmapConfig.androidKey, AmapConfig.iosKey);
+    _setLocationOption();
+    _locationPlugin.startLocation();
+
+    _locationPlugin.onLocationChanged().listen((Map<String, Object>event) {
+
+      print("11111111111");
+      print(event);
+    });
+  }
+
+
+  ///设置定位参数
+  void _setLocationOption() {
+    if (null != _locationPlugin) {
+      AMapLocationOption locationOption = new AMapLocationOption();
+
+      ///是否单次定位
+      locationOption.onceLocation = true;
+
+      ///是否需要返回逆地理信息
+      locationOption.needAddress = true;
+
+      ///逆地理信息的语言类型
+      locationOption.geoLanguage = GeoLanguage.DEFAULT;
+
+      locationOption.desiredLocationAccuracyAuthorizationMode =
+          AMapLocationAccuracyAuthorizationMode.ReduceAccuracy;
+
+      locationOption.fullAccuracyPurposeKey = "AMapLocationScene";
+
+      ///设置Android端连续定位的定位间隔
+      locationOption.locationInterval = 2000;
+
+      ///设置Android端的定位模式<br>
+      ///可选值：<br>
+      ///<li>[AMapLocationMode.Battery_Saving]</li>
+      ///<li>[AMapLocationMode.Device_Sensors]</li>
+      ///<li>[AMapLocationMode.Hight_Accuracy]</li>
+      locationOption.locationMode = AMapLocationMode.Hight_Accuracy;
+
+      ///设置iOS端的定位最小更新距离<br>
+      locationOption.distanceFilter = -1;
+
+      ///设置iOS端期望的定位精度
+      /// 可选值：<br>
+      /// <li>[DesiredAccuracy.Best] 最高精度</li>
+      /// <li>[DesiredAccuracy.BestForNavigation] 适用于导航场景的高精度 </li>
+      /// <li>[DesiredAccuracy.NearestTenMeters] 10米 </li>
+      /// <li>[DesiredAccuracy.Kilometer] 1000米</li>
+      /// <li>[DesiredAccuracy.ThreeKilometers] 3000米</li>
+      locationOption.desiredAccuracy = DesiredAccuracy.Best;
+
+      ///设置iOS端是否允许系统暂停定位
+      locationOption.pausesLocationUpdatesAutomatically = false;
+
+      ///将定位参数设置给定位插件
+      _locationPlugin.setLocationOption(locationOption);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-
+//CameraPosition
     final AMapWidget map = AMapWidget(
       apiKey: AmapConfig.amapApiKeys,
       privacyStatement: AmapConfig.amapPrivacyStatement,
       onMapCreated: onMapCreated,
+      scaleEnabled: false,
+      myLocationStyleOptions: MyLocationStyleOptions(true),
+      initialCameraPosition: const CameraPosition(target: LatLng(38.047843, 114.520791), zoom: 17),
+      // onLocationChanged: (location) {
+      //
+      //   print("lat = " + location.latLng.latitude.toString());
+      //   print("lng = " + location.latLng.longitude.toString());
+      // },
+      onCameraMoveEnd: (pos) {
+
+        print("lat = " + pos.target.latitude.toString());
+        print("lng = " + pos.target.longitude.toString());
+        _mapCenter =
+            LatLng(pos.target.latitude, pos.target.longitude);
+      },
+
     );
 
     return Scaffold(
@@ -47,17 +130,17 @@ class _SelectLocationState extends State<SelectLocation>
               height: 500.h,
               child: map,
             ),
-            Positioned(
-              right: 10,
-                bottom: 15,
-                child: Container(
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: _approvalNumberWidget,
-                  ),
-                )
-            )
+            // Positioned(
+            //   right: 10,
+            //     bottom: 15,
+            //     child: Container(
+            //       alignment: Alignment.centerLeft,
+            //       child: Column(
+            //         mainAxisAlignment: MainAxisAlignment.start,
+            //         children: _approvalNumberWidget,
+            //       ),
+            //     )
+            // )
 
           ],
         ),
